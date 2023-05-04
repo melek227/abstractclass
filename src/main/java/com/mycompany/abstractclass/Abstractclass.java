d77779e4595bf05d76a54edf66f8dc34e53908b0
melek277 <melek_yilmaz_1997@hotmail.com>
2023-05-04 11:43:46 +0300

    Initial commit

---
+++ src/main/java/com/mycompany/abstractclass/Abstractclass.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.abstractclass;
+
+/**
+ *
+ * @author melek
+ */
+public class Abstractclass {
+
+    public static void main(String[] args) {
+        BaseCalculator calculator=new WomenCalculator();
+        
+    }
+}
